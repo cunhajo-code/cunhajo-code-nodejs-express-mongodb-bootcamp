f1e063a3d3db5f1902ae4a41d06341d4820e6732
Jose Cunha <cunhajo_code@att.net>
2025-05-06 17:34:53 -0400

    Refactored once again to move APIFeatures class to a APIFeatrues.js in utils file, export and require in the tour controller. All old commented code regions removed, remain in the back version of the toursController file.

--- 4-natours/written/controllers/tourController.js
+++ 4-natours/written/controllers/tourController.js
@@ -1,108 +1,6 @@
 /* eslint-disable no-unused-vars */
 const Tour = require('./../models/tourModel');
-
-// #region removed code, used before MongoDB Mongoose code
-/*
- const tours = JSON.parse(
-    // eslint-disable-next-line no-undef
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
-  exports.CheckID = (req, resp, next, val) => {
-    console.log(`Tour id is : ${val}`);
-    if (req.params.id * 1 > tours.length) {
-      return resp.status(404).json({
-        status: 'fail',
-        message: 'tour not found',
-      });
-    }
-    next();
-  };
-*/
-// #endregion
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-    return this;
-  }
-  filter() {
-    console.log(this.queryString);
-
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['sort', 'fields', 'page', 'limit'];
-
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    const filterObj = JSON.parse(
-      JSON.stringify(queryObj).replace(
-        /\b(gte|gt|lte|lt)\b/g,
-        (match) => `$${match}`
-      )
-    );
-
-    // ⬇ Assign filterObj to both the query and a new property
-    this.query = this.query.find(filterObj);
-    this.filterObj = filterObj; // <-- this is what you'll use for countDocuments()
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      //add sort criteria from url, replacing commas with spaces
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      //set default sort of created
-      this.query = this.query.sort('-createdAt');
-    }
-    return this;
-  }
-
-  selection() {
-    if (this.queryString.fields) {
-      //include ONLY the selected field names
-      //id is also sent
-      const fields = this.queryString.fields.split(',').join(' ');
-      //console.log(fields);
-      this.query = this.query.select(fields);
-    } else {
-      //exclude the fields named prefixed with a minus
-      // in this case, always exlude th MogoDB generated __v field
-      this.query = this.query.select('-__v');
-    }
-    return this;
-  }
-
-  pagination(rowCount) {
-    // console.log(`pagination call results count= ${results}`);
-    if (this.queryString.page || this.queryString.limit) {
-      // counts the records AFTER the query is applied
-      const numTours = rowCount;
-
-      // page intialized to one if not specified with nullish coalescing operator
-      const rawPage = this.queryString.page ?? 1;
-      const page = rawPage * 1;
-      const limit = this.queryString.limit * 1;
-      const lastPage = Math.ceil(numTours / limit);
-      let targetPage = 1;
-
-      if (page > 1 && page <= lastPage) {
-        targetPage = page;
-      } else if (page > lastPage) {
-        targetPage = lastPage;
-      } else if (page < 1) {
-        targetPage = 1;
-      }
-
-      const skip = (targetPage - 1) * limit;
-      this.query = this.query.skip(skip).limit(limit);
-    }
-
-    return this;
-  }
-}
+const APIFeatures = require('./utils/apiFeatures');
 
 // tours handlers
 exports.AliasTop5CheapTours = (req, resp, next) => {
@@ -149,13 +47,6 @@ exports.getAllTours = async (req, resp) => {
 };
 
 exports.getTourById = async (req, resp) => {
-  // #region old create standin new id code prior to mongoose code and model
-
-  // const id = req.params.id * 1;
-  // const tour = tours.find((el) => el.id === id);
-
-  // #endregion
-
   try {
     const tour = await Tour.findById(req.params.id);
 
@@ -195,29 +86,6 @@ exports.updateTour = async (req, resp) => {
 };
 
 exports.createTour = async (req, resp) => {
-  // #region old createTour Logic pre Mongoose and Schema
-  /*
-   console.log(req.body);
-  pseudo code to fake getting a new id for the elements in the tours file we are using
-   const newId = tours[tours.length - 1].id + 1;
-   const newTour = Object.assign({ id: newId }, req.body);
-   tours.push(newTour);
-   fs.writeFile(
-     // eslint-disable-next-line no-undef
-     `${__dirname}/dev-data/data/tours-simple.json`,
-     JSON.stringify(tours),
-     (err) => {
-       resp.status(201).json({
-         status: 'success',
-         data: {
-           tour: newTour,
-         },
-       });
-     }
-   );
-  */
-  // #endregion
-
   try {
     const newTour = await Tour.create(req.body);
 
---
+++ 4-natours/written/controllers/utils/apiFeatures.js
@@ -0,0 +1,85 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+    return this;
+  }
+  filter() {
+    console.log(this.queryString);
+
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['sort', 'fields', 'page', 'limit'];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const filterObj = JSON.parse(
+      JSON.stringify(queryObj).replace(
+        /\b(gte|gt|lte|lt)\b/g,
+        (match) => `$${match}`
+      )
+    );
+
+    // ⬇ Assign filterObj to both the query and a new property
+    this.query = this.query.find(filterObj);
+    this.filterObj = filterObj; // <-- this is what you'll use for countDocuments()
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      //add sort criteria from url, replacing commas with spaces
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      //set default sort of created
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+
+  selection() {
+    if (this.queryString.fields) {
+      //include ONLY the selected field names
+      //id is also sent
+      const fields = this.queryString.fields.split(',').join(' ');
+      //console.log(fields);
+      this.query = this.query.select(fields);
+    } else {
+      //exclude the fields named prefixed with a minus
+      // in this case, always exlude th MogoDB generated __v field
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  pagination(rowCount) {
+    // console.log(`pagination call results count= ${results}`);
+    if (this.queryString.page || this.queryString.limit) {
+      // counts the records AFTER the query is applied
+      const numTours = rowCount;
+
+      // page intialized to one if not specified with nullish coalescing operator
+      const rawPage = this.queryString.page ?? 1;
+      const page = rawPage * 1;
+      const limit = this.queryString.limit * 1;
+      const lastPage = Math.ceil(numTours / limit);
+      let targetPage = 1;
+
+      if (page > 1 && page <= lastPage) {
+        targetPage = page;
+      } else if (page > lastPage) {
+        targetPage = lastPage;
+      } else if (page < 1) {
+        targetPage = 1;
+      }
+
+      const skip = (targetPage - 1) * limit;
+      this.query = this.query.skip(skip).limit(limit);
+    }
+
+    return this;
+  }
+}
+
+modules.export = APIFeatures;
